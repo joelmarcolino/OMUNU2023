8511a119bff95690fe70180f3b3b1dcee96e4afd
= <=>
2023-02-20 21:58:04 +0100

    Homepage fixed

---
+++ src/pages/Homepage/Home.page.style.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Description = styled.div`
+  position: relative;
+  margin-top: 200px;
+  height: 550px;
+  background-image: url("peopleReadding.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  justify-content: center;
+  display: flex;
+  margin-bottom: 20px;
+  .content {
+    position: absolute;
+    bottom: 0;
+  }
+`;
--- src/pages/Homepage/Home.page.tsx
+++ src/pages/Homepage/Home.page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  BtnDefaultComponent,
+  BtnPrimaryComponent,
+  BtnSecondaryComponent,
+  BtnWarmComponent,
+  CircleComponent,
+  HomeNavbarComponent,
+  MyImageComponent,
+} from "../../components/Components";
+import { Description } from "./Home.page.style";
 
 export default function HomePage() {
   return (
     <>
-        Home Page 
+      <div className="container-fluid p-0">
+        <HomeNavbarComponent />
+
+        <CircleComponent
+          image={"/Ellipse1.png"}
+          width={"450px"}
+          height={"350px"}
+          top={""}
+          type={"default"}
+        />
+      </div>
+      <div>
+        <CircleComponent
+          image={"/Ellipse2.png"}
+          width={"100px"}
+          height={"400px"}
+          top={"320px"}
+        />
+      </div>
+      <div className="container">
+
+        <Description className="col-md-12 col-sm-12">
+
+          <div className="content ">
+          <BtnPrimaryComponent desc={"Autenticar-se"} _className={""} icon={""} />
+          <BtnWarmComponent desc={"Criar Conta"} _className={"ms-3"}/>
+          </div>
+          
+        </Description>
+
+       
+      </div>
     </>
-  )
+  );
 }
