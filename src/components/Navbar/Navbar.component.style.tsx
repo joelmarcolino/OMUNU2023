e86710e4c832c562acb7258dc44bc61b299d0b81
= <=>
2023-02-20 21:57:37 +0100

    Navbar fixed

--- src/components/Navbar/Navbar.component.style.tsx
+++ src/components/Navbar/Navbar.component.style.tsx
@@ -1,23 +1,23 @@
 import styled from "styled-components";
 
+const Navbar = styled.div`
+  box-shadow: 0px 0px 1px #385A64;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
 const Menu = styled.ul`
-    .nav-link{
-        color: #ffffffc5 !important;
-        font-size: 1.1rem;
+  .nav-link {
+    color: #385A64 !important;
+    font-size: 1.1rem;
+  }
 
-    }
+  .active {
+    color: #385A64 !important;
+    font-weight: 550 !important;
+  }
+`;
 
-    .active{
-        color: #fff !important;
-        font-weight: 550 !important;
-    }
+const Item = styled.li``;
 
-`
-
-const Item = styled.li`
-    
-`
-
-export {
-    Menu, Item
-}
+export { Menu, Item, Navbar };
--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,82 +1,77 @@
-import { BtnSecondary, MyImageComponent } from "@/components/components";
 import Link from "next/link";
 import React, { useState } from "react";
+import { BtnSecondaryComponent, MyImageComponent } from "../Components";
 import { navMenu } from "../types/header.type";
-import { Item, Menu } from "./Navbar.component.style";
+import { Item, Menu, Navbar } from "./Navbar.component.style";
 
-export function HomeNavbar() {
-  return <div>HomeNavbar</div>;
-}
-
-export function HomeNavbarTransparent() {
+export function HomeNavbarComponent() {
   const [menu, updateMenu] = useState<Array<navMenu>>([
     {
       desc: "Home",
       active: true,
     },
     {
-      desc: "Páginas",
+      desc: "Livros",
       active: false,
     },
     {
-      desc: "Serviços",
-      active: false,
-    },
-    {
-      desc: "Preçário",
-      active: false,
-    },
-    {
-      desc: "Adesão",
+      desc: "Sobre",
       active: false,
     },
   ]);
 
-  const btnDescription = "Entrar";
+  const btnDescription = "Comunidade de apoio";
 
   return (
-    <div className="row">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <MyImageComponent name={"/logo.png"} width={210} />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+    <>
+      <Navbar>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <MyImageComponent name={"/Logo omomu.png"} width={200} height={200}/>
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <Menu className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-              {menu.map((item, index) => {
-                return (
-                  <>
-                    <Item className="nav-item">
-                      <Link
-                        className={ item.active ? "nav-link active" : "nav-link"}
-                        href="#"
-                      >
-                        {item.desc}
-                      </Link>
-                    </Item>
-                  </>
-                );
-              })}
-            </Menu>
-
-            <div className="d-flex" role="search">
-              <BtnSecondary desc={btnDescription} w="125px" h="40px" />
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <Menu className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+                {menu.map((item, index) => {
+                  return (
+                    <>
+                      <Item className="nav-item ps-4 pe-4">
+                        <Link
+                          className={
+                            item.active ? "nav-link active" : "nav-link"
+                          }
+                          href="#"
+                        >
+                          {item.desc}
+                        </Link>
+                      </Item>
+                    </>
+                  );
+                })}
+              </Menu>
             </div>
+            
           </div>
-        </div>
-      </nav>
-    </div>
+          <div className="d-flex" role="search">
+              <BtnSecondaryComponent desc={btnDescription} w="245px" h="40px" />
+            </div>
+        </nav>
+      </Navbar>
+    </>
   );
 }
