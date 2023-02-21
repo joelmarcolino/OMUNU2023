d0061ba36dce27f5661886736183dad37d1144ba
= <=>
2023-02-21 09:17:34 +0100

    Buttons fixed

--- src/components/Buttons/Buttons.component.style.tsx
+++ src/components/Buttons/Buttons.component.style.tsx
@@ -5,38 +5,36 @@ const ButtonDefault = styled.button<any>`
 
   height: ${(props) => props.h};
 
-font-weight: 400;
+  font-weight: 400;
+
 `;
 
 const ButtonSecondary = styled(ButtonDefault)`
-    background-color: #F4F4F4 !important;
-    color: #385A64;
+  background-color: #f4f4f4 !important;
+  color: #385a64;
 
-    &:hover{
-        background-color: #385A64 !important;
-        color: #F4F4F4;
-    }
-    
-`
+  &:hover {
+    background-color: #385a64 !important;
+    color: #f4f4f4;
+  }
+`;
 const ButtonPrimary = styled(ButtonDefault)`
-    background-color: #385A64 !important;
-    color: #F4F4F4;
+  background-color: #385a64 !important;
+  color: #f4f4f4;
 
-    &:hover{
-        background-color: #2e4d57 !important;
-        color: #ffffff;
-    }
-    
-`
+  &:hover {
+    background-color: #2e4d57 !important;
+    color: #ffffff;
+  }
+`;
 const ButtonWarm = styled(ButtonDefault)`
-    background-color:#f5c105  !important;
-    color: #fff;
+  background-color: #f5c105 !important;
+  color: #fff;
 
-    &:hover{
-        background-color: #FFC704 !important;
-        color: #ffffff;
-    }
-    
-`
+  &:hover {
+    background-color: #ffc704 !important;
+    color: #ffffff;
+  }
+`;
 
 export { ButtonDefault, ButtonSecondary, ButtonPrimary, ButtonWarm };
