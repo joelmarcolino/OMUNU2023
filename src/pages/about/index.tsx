cbb80f406acdc78df61f6cc7b9365cade0f06180
= <=>
2023-02-21 07:08:03 +0100

    About page done

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  CircleBottomComponent,
+  CircleComponent,
+  HeadingComponent,
+  HomeNavbarComponent,
+} from "../../components/Components";
+import { DescriptionImg, CardAbout } from "./index.style";
+
+export default function About() {
+  const description =
+    "Entra na onda e descobre a magia infantil com frases e livros para alunos vinculado no âmbito da educação para o desenvolvimento de escrituras dando assim aberturas para leitores infantis no contexto e nos exercícios propostos na educação infantil, com a finalmente de criar crianças com incentivo de inovação no processo da educação.";
+
+  return (
+    <>
+      <HomeNavbarComponent />
+      <div className="container-fluid">
+        <CircleComponent
+          image={"/Ellipse4.png"}
+          width={"100px"}
+          height={"400px"}
+          top={""}
+        />
+
+        <CircleBottomComponent
+          image={"/Ellipse5.png"}
+          width={"500px"}
+          height={"400px"}
+        />
+      </div>
+      <div className="container">
+        <CardAbout className="row">
+          <div className="col-md-12">
+            <HeadingComponent type={"h1"}>Omunu</HeadingComponent>
+          </div>
+          <div className="col-sm-6 col-md-6 pt-5">
+            <HeadingComponent type={"text"}>{description}</HeadingComponent>
+          </div>
+          <div className="col-sm-6 col-md-6">
+            <DescriptionImg></DescriptionImg>
+          </div>
+        </CardAbout>
+      </div>
+    </>
+  );
+}
