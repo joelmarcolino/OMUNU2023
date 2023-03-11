01d2a1a268964f3c0b21fe9b3573e599024f7beb
Elísio Mualumene <85412882+elisiomualumene@users.noreply.github.com>
2023-03-11 23:10:00 +0000

    :rocket:

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,17 +2,26 @@ import Link from "next/link"
 import { Box, List, ListItem } from "@mui/material"
 import { ImageComponent } from "../Image/Image"
 import logo from "../../../public/omunu_logo.png"
-import { Circle } from "../Solids Effects/Circle"
+import {Button as MuiButton, ButtonProps} from "@mui/material"
+import { styled } from '@mui/material/styles';
+import { yellow } from '@mui/material/colors';
 
 export const Navbar = () => {
+
+  const YellowToBlue = styled(MuiButton)<ButtonProps>(({ theme }) => ({
+    color: theme.palette.getContrastText(yellow[400]),
+    backgroundColor: "#385A64",
+    '&:hover': {
+      backgroundColor: "#385A65",
+    },
+  }));
+
   return (
-    <Box sx={{display: "flex"}}>
-      <Circle background="#385A64" right="-8rem" top="-4rem"/>
-      <Circle background="#FFC704" right="-15rem" top="10rem"/>
-      <Box sx={{m: "1rem"}}>
+    <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+      <Box>
         <ImageComponent name={logo} height={900} width={150}/>
       </Box>
-      <List sx={{display: "flex", cursor: "pointer", ml: "18rem"}}>
+      <List sx={{display: "flex", cursor: "pointer"}}>
         <ListItem sx={{fontSize: "20px", fontWeight: "bold"}}>
         <Link href="/" style={{textDecoration: "none", color: "#385A64"}}>Home</Link>
         </ListItem>
@@ -23,6 +32,44 @@ export const Navbar = () => {
         <Link href="/about" style={{textDecoration: "none", color: "#385A64"}}>sobre</Link>
         </ListItem>
       </List>
+      <Box>
+      <YellowToBlue
+          variant="contained"
+          sx={{
+            height: "40px",
+            color: "white",
+            fontWeight: "bold",
+            m: ".2rem",
+            borderRadius: "10px"
+          }}>
+              <Link
+                href="/signup"
+                style={{
+                  textDecoration: "none",
+                  color: "white"
+          }}>
+            Entrar
+              </Link>
+        </YellowToBlue>
+        <YellowToBlue
+          variant="contained"
+          sx={{
+            height: "40px",
+            color: "white",
+            fontWeight: "bold",
+            m: ".2rem",
+            borderRadius: "10px"
+          }}>
+              <Link
+                href="/signup"
+                style={{
+                  textDecoration: "none",
+                  color: "white"
+          }}>
+            Criar Conta
+              </Link>
+        </YellowToBlue>
+      </Box>
     </Box>
   )
 }
